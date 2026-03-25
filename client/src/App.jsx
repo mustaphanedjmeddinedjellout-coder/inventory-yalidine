73af5ee555bdfef80fa19e1c1520b7ac7bf7f187
nedjme055 <146088529+nedjme055@users.noreply.github.com>
2026-03-25 17:43:45 +0100

    Fix deploy install resolution and add Meta Pixel tracker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation, useParams } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
@@ -15,6 +16,50 @@ import { CartProvider } from './storefront/cart-context';
 import AdminGuard from './admin/AdminGuard';
 import AdminLogin from './admin/AdminLogin';
 
+const META_PIXEL_ID = '4332650283655718';
+
+function MetaPixelTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    if (window.fbq) return;
+
+    /* eslint-disable no-underscore-dangle */
+    (function initMetaPixel(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function fbqProxy() {
+        if (n.callMethod) {
+          n.callMethod.apply(n, arguments);
+        } else {
+          n.queue.push(arguments);
+        }
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js'));
+    /* eslint-enable no-underscore-dangle */
+
+    window.fbq('init', META_PIXEL_ID);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !window.fbq) return;
+    if (location.pathname.startsWith('/admin')) return;
+    window.fbq('track', 'PageView');
+  }, [location.pathname]);
+
+  return null;
+}
+
 function ProductRoute() {
   const { slug } = useParams();
   return <Product key={slug} />;
@@ -24,6 +69,7 @@ function App() {
   return (
     <BrowserRouter>
       <CartProvider>
+        <MetaPixelTracker />
         <Routes>
           <Route element={<StorefrontLayout />}>
             <Route path="/" element={<Home />} />
